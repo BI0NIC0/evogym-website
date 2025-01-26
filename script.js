ab4a41470de441d2f0ab24d3a0d161fdb19b43de
BI0NIC0 <76895157+BI0NIC0@users.noreply.github.com>
2025-01-26 10:59:01 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    console.log('Evogym website loaded successfully!');
+    ScrollReveal().reveal('.reveal', {
+        distance: '50px',
+        duration: 800,
+        easing: 'ease-in-out',
+        origin: 'bottom',
+        interval: 200
+    });
 });
